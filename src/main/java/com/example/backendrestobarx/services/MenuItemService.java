ed8081140b8438c4f3a7f50e65cf3fe699c5c769
Marx Alonso <marxchip99@gmail.com>
2025-09-16 13:21:16 -0500

    add menuitemservice

--- src/main/java/com/example/backendrestobarx/services/MenuItemService.java
+++ src/main/java/com/example/backendrestobarx/services/MenuItemService.java
@@ -1,4 +1,28 @@
 package com.example.backendrestobarx.services;
 
+import com.example.backendrestobarx.entity.MenuItem;
+import com.example.backendrestobarx.repository.MenuItemRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MenuItemService {
+    private final MenuItemRepository menuItemRepository;
+
+    public MenuItemService(MenuItemRepository menuItemRepository) {
+        this.menuItemRepository = menuItemRepository;
+    }
+
+    public List<MenuItem> getAll() {
+        return menuItemRepository.findAll();
+    }
+
+    public List<MenuItem> getByCategory(Long categoryId) {
+        return menuItemRepository.findByCategoryId(categoryId);
+    }
+
+    public MenuItem save(MenuItem item) {
+        return menuItemRepository.save(item);
+    }
 }
